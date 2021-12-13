ca87f8d1c8edd9f692331f00791e49ab283ca4d7
yangxu <yangxu@tsign.cn>
2021-12-13 16:48:39 +0800

    添加产品线和业务域的VO

---
+++ facade/src/main/java/com/timevale/forward/facade/api/result/BizDomainVO.java
@@ -0,0 +1,30 @@
+package com.timevale.forward.facade.api.result;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import com.timevale.mandarin.common.result.ToString;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * @author by YangXu
+ * @Date 2021/12/13 16:38
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+@ApiModel("业务域")
+public class BizDomainVO extends ToString {
+
+    @ApiModelProperty("id")
+    @JsonSerialize(using = ToStringSerializer.class)
+    private Long id;
+
+    @ApiModelProperty("名称")
+    private String name;
+
+    @ApiModelProperty("负责人")
+    private String owner;
+
+}
---
+++ facade/src/main/java/com/timevale/forward/facade/api/result/ProductLineVO.java
@@ -0,0 +1,34 @@
+package com.timevale.forward.facade.api.result;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import com.timevale.mandarin.common.result.ToString;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+
+/**
+ * @author by YangXu
+ * @Date 2021/12/13 16:36
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+@ApiModel("产品线")
+public class ProductLineVO extends ToString {
+
+    @ApiModelProperty("id")
+    @JsonSerialize(using = ToStringSerializer.class)
+    private Long id;
+
+    @ApiModelProperty("业务域id")
+    @JsonSerialize(using = ToStringSerializer.class)
+    private Long bizDomainId;
+
+    @ApiModelProperty("名称")
+    private String name;
+
+    @ApiModelProperty("负责人")
+    private String owner;
+}
