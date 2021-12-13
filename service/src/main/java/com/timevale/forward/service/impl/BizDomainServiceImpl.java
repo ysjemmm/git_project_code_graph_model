71f7cd7b235f8c97201faff38e2d71bfab9d232d
yangxu <yangxu@tsign.cn>
2021-12-13 17:45:35 +0800

    添加业务域注释

--- service/src/main/java/com/timevale/forward/service/impl/BizDomainServiceImpl.java
+++ service/src/main/java/com/timevale/forward/service/impl/BizDomainServiceImpl.java
@@ -3,6 +3,8 @@ package com.timevale.forward.service.impl;
 import com.timevale.footstone.base.model.response.BaseResult;
 import com.timevale.forward.facade.api.client.BizDomainService;
 import com.timevale.forward.facade.api.result.BizDomainVO;
+import com.timevale.mandarin.common.annotation.RestService;
+import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.util.Lists;
 
 import java.util.List;
@@ -11,6 +13,8 @@ import java.util.List;
  * @author by YangXu
  * @Date 2021/12/13 17:10
  */
+@Slf4j
+@RestService
 public class BizDomainServiceImpl implements BizDomainService {
 
     @Override
