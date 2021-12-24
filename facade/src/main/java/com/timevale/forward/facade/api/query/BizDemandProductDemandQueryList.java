e3e4d0435fcbe2dff7991d749d4a8c8994b4e84d
yangxu <yangxu@tsign.cn>
2021-12-24 11:07:50 +0800

    修改部分sql判断

--- facade/src/main/java/com/timevale/forward/facade/api/query/BizDemandProductDemandQueryList.java
+++ facade/src/main/java/com/timevale/forward/facade/api/query/BizDemandProductDemandQueryList.java
@@ -6,6 +6,8 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+import javax.validation.constraints.NotNull;
+
 /**
  * @author by YangXu
  * @date 2021/12/24 10:39
@@ -16,5 +18,6 @@ import lombok.EqualsAndHashCode;
 public class BizDemandProductDemandQueryList extends QueryBase {
 
     @ApiModelProperty("业务需求id")
+    @NotNull(message = "业务需求id不能为空")
     private Long bizDemandId;
 }
