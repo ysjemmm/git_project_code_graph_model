6c1bf92e6c643de07683b3ba10c2f8dc14a9b08c
yangxu <yangxu@tsign.cn>
2022-02-08 14:09:26 +0800

    计算项目成员参与的项目数

--- service/src/main/java/com/timevale/forward/service/impl/HomePageServiceImpl.java
+++ service/src/main/java/com/timevale/forward/service/impl/HomePageServiceImpl.java
@@ -97,7 +97,7 @@ public class HomePageServiceImpl implements HomePageService {
         int bizDemandCount = 0;
 
         // 获取我及所有下属
-        List<String> allMyStaffWithSelf = innerUserPersonClient.getAllMyStaffWithSelf("yangxu");
+        List<String> allMyStaffWithSelf = innerUserPersonClient.getAllMyStaffWithSelf(userInfo.getId());
 
         // 进行中的项目
         projectCount = projectMapper.countByTeamMember(allMyStaffWithSelf);
