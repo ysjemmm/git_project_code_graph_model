752ebe8dc1d864c07091c03469bbc2696b495414
yangxu <yangxu@tsign.cn>
2022-02-10 15:22:44 +0800

    fix:风险预警修复传入空值，导致查询全部数据

--- service/src/main/java/com/timevale/forward/service/impl/HomePageServiceImpl.java
+++ service/src/main/java/com/timevale/forward/service/impl/HomePageServiceImpl.java
@@ -93,7 +93,7 @@ public class HomePageServiceImpl implements HomePageService {
         int bizDemandCount = 0;
 
         // 获取我及所有下属
-        List<String> allMyStaffWithSelf = innerUserPersonClient.getAllMyStaffWithSelf("yangxu");
+        List<String> allMyStaffWithSelf = innerUserPersonClient.getAllMyStaffWithSelf(userInfo.getId());
 
         // 进行中的项目
         List<ProjectDO> projectDOList = projectMapper.selectByTeamMember(allMyStaffWithSelf);
