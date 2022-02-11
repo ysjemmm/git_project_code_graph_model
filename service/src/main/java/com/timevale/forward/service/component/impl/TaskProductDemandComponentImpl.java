988b2d277507ce4cacaa00703a608c213f1e1f8b
xingyun <xingyun@tsign.cn>
2022-02-11 14:38:45 +0800

    取消关联

--- service/src/main/java/com/timevale/forward/service/component/impl/TaskProductDemandComponentImpl.java
+++ service/src/main/java/com/timevale/forward/service/component/impl/TaskProductDemandComponentImpl.java
@@ -2,17 +2,14 @@ package com.timevale.forward.service.component.impl;
 
 import com.timevale.forward.dal.condition.TaskProductDemandCondition;
 import com.timevale.forward.dal.dao.TaskProductDemandMapper;
-import com.timevale.forward.dal.entity.ProductDemandListDO;
 import com.timevale.forward.dal.entity.TaskProductDemandDO;
 import com.timevale.forward.dal.entity.TaskProductDemandUpdateDO;
-import com.timevale.forward.model.enums.TaskStatusEnum;
 import com.timevale.forward.service.component.TaskProductDemandComponent;
 import com.timevale.forward.service.constant.CommonConstant;
 import com.timevale.forward.service.utils.envoy.LocalSessionUtils;
 import com.timevale.forward.service.utils.envoy.UserInfo;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections.CollectionUtils;
-import org.assertj.core.util.Lists;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
@@ -67,30 +64,30 @@ public class TaskProductDemandComponentImpl implements TaskProductDemandComponen
         taskProductDemandMapper.update(taskProductDemandDO);
     }
 
-    /**
-     * 任务对应的产品需求状态全部变更为已暂停/作废时时，取消关联任务
-     *
-     * @param productDemandId productDemandId
-     */
-    @Override
-    public void unLinkIfProductDemandStatusAllChange(Long productDemandId) {
-        TaskProductDemandCondition c = TaskProductDemandCondition.builder().productDemandId(productDemandId).build();
-        List<TaskProductDemandDO> taskProductDemandDO = taskProductDemandMapper.get(c);
-        if (CollectionUtils.isEmpty(taskProductDemandDO)) {
-            return;
-        }
-        List<Long> taskIds = taskProductDemandDO.stream().map(TaskProductDemandDO::getTaskId).collect(Collectors.toList());
-        log.info("产品需求productDemandId:{},关联的任务taskIds:{}", productDemandId, taskIds);
-        taskIds.forEach(a -> {
-            List<Integer> list = taskProductDemandMapper.linkProductDemandList(a).stream()
-                    .map(ProductDemandListDO::getStatus)
-                    .filter(status -> (!status.equals(TaskStatusEnum.SUSPEND.getCode())
-                            && !status.equals(TaskStatusEnum.INVALID.getCode()))).collect(Collectors.toList());
-            if (CollectionUtils.isEmpty(list)) {
-                //任务关联的产品需求全部作废或暂停
-                log.info("需要取消关联的任务taskId:{}", a);
-                update(Lists.newArrayList(a), null);
-            }
-        });
-    }
+//    /**
+//     * 任务对应的产品需求状态全部变更为已暂停/作废时时，取消关联任务
+//     *
+//     * @param productDemandId productDemandId
+//     */
+//    @Override
+//    public void unLinkIfProductDemandStatusAllChange(Long productDemandId) {
+//        TaskProductDemandCondition c = TaskProductDemandCondition.builder().productDemandId(productDemandId).build();
+//        List<TaskProductDemandDO> taskProductDemandDO = taskProductDemandMapper.get(c);
+//        if (CollectionUtils.isEmpty(taskProductDemandDO)) {
+//            return;
+//        }
+//        List<Long> taskIds = taskProductDemandDO.stream().map(TaskProductDemandDO::getTaskId).collect(Collectors.toList());
+//        log.info("产品需求productDemandId:{},关联的任务taskIds:{}", productDemandId, taskIds);
+//        taskIds.forEach(a -> {
+//            List<Integer> list = taskProductDemandMapper.linkProductDemandList(a).stream()
+//                    .map(ProductDemandListDO::getStatus)
+//                    .filter(status -> (!status.equals(TaskStatusEnum.SUSPEND.getCode())
+//                            && !status.equals(TaskStatusEnum.INVALID.getCode()))).collect(Collectors.toList());
+//            if (CollectionUtils.isEmpty(list)) {
+//                //任务关联的产品需求全部作废或暂停
+//                log.info("需要取消关联的任务taskId:{}", a);
+//                update(Lists.newArrayList(a), null);
+//            }
+//        });
+//    }
 }
