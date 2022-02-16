1b14bf06aecb1ccc9f40106e626de27b304ac352
xingyun <xingyun@tsign.cn>
2022-02-16 10:27:04 +0800

    单测

---
+++ deploy/src/test/java/com/timevale/forward/service/impl/ProjectServiceImplTest.java
@@ -0,0 +1,19 @@
+package com.timevale.forward.service.impl;
+
+import org.mockito.InjectMocks;
+import org.springframework.boot.test.mock.mockito.MockitoTestExecutionListener;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+
+/**
+ * @author xingyun
+ * @date 2021-12-13 13:58
+ **/
+@TestExecutionListeners(listeners = MockitoTestExecutionListener.class)
+public class ProjectServiceImplTest extends AbstractTestNGSpringContextTests {
+
+    @InjectMocks
+    private ProjectServiceImpl projectService;
+
+
+}
