8be74a54bf56605109ccd15cc772be43b3078dba
xingyun <xingyun@tsign.cn>
2022-02-23 17:53:37 +0800

    bug管理

--- service/src/main/java/com/timevale/forward/service/impl/BugOfflineServiceImpl.java
+++ service/src/main/java/com/timevale/forward/service/impl/BugOfflineServiceImpl.java
@@ -124,12 +124,13 @@ public class BugOfflineServiceImpl implements BugOfflineService {
     }
 
     @Override
+    @Transactional(rollbackFor = Exception.class)
     public BaseResult<Boolean> transfer(BugOfflineTransferReq bugOfflineTransferReq) {
-        //1.验证操作人是否是经办人或其上级,状态是否是bug打开,待修复,待确认,待验收
+        //1.验证操作人是否是经办人或其上级,状态是否是bug打开,待修复,延期修复,待确认,待验收
 
         //2.查找bug数据
 
-        //3.赋值:经办人为表单选择人,上一阶段经办人不变(测试角色),状态不变
+        //3.赋值:经办人为表单选择人,上一阶段经办人不变,状态不变
 
         //4.更新bug数据
 
@@ -138,12 +139,13 @@ public class BugOfflineServiceImpl implements BugOfflineService {
     }
 
     @Override
+    @Transactional(rollbackFor = Exception.class)
     public BaseResult<Boolean> unHandle(Long id) {
         //1.验证操作人是否是经办人或其上级,状态是否是bug打开
 
         //2.查找bug数据
 
-        //3.赋值:经办人为bug表中上一阶段经办人(测试角色),上一阶段经办人赋值为本次操作人,状态变成待确认
+        //3.赋值:经办人为bug提出人,上一阶段经办人赋值为新增bug时经办人,状态变成待确认
 
         //4.更新bug数据
 
@@ -154,12 +156,13 @@ public class BugOfflineServiceImpl implements BugOfflineService {
     }
 
     @Override
+    @Transactional(rollbackFor = Exception.class)
     public BaseResult<Boolean> agree(Long id) {
         //1.验证操作人是否是经办人或其上级,状态是否是待确认
 
         //2.查找bug数据
 
-        //3.赋值:状态变成关闭,经办人(测试角色)与上一阶段经办人(开发角色)不变,
+        //3.赋值:状态变成关闭,经办人与上一阶段经办人不变,
 
         //4.更新bug数据
 
@@ -168,12 +171,13 @@ public class BugOfflineServiceImpl implements BugOfflineService {
     }
 
     @Override
+    @Transactional(rollbackFor = Exception.class)
     public BaseResult<Boolean> reject(Long id) {
         //1.验证操作人是否是经办人或其上级,状态是否是待确认
 
         //2.查找bug数据
 
-        //3.赋值:经办人为bug表中上一阶段经办人(开发角色),上一阶段经办人为本次操作人,状态变成bug打开
+        //3.赋值:经办人为不用修复时上一阶段经办人,上一阶段经办人赋值为本次经办人(经办人与上一阶段经办人交换位置),状态变成bug打开
 
         //4.更新bug数据
 
@@ -182,12 +186,13 @@ public class BugOfflineServiceImpl implements BugOfflineService {
     }
 
     @Override
+    @Transactional(rollbackFor = Exception.class)
     public BaseResult<Boolean> delayHandle(Long id) {
         //1.验证操作人是否是经办人或其上级,状态是否是bug打开或待修复
 
         //2.查找bug数据
 
-        //3.赋值:经办人为bug表中上一阶段经办人(测试角色),上一阶段经办人为本次操作人,状态变成延期修复,取消关联项目
+        //3.赋值:经办人为bug提出人,上一阶段经办人赋值为新增bug时经办人,状态变成延期修复,取消关联项目
 
         //4.更新bug数据
 
@@ -196,12 +201,13 @@ public class BugOfflineServiceImpl implements BugOfflineService {
     }
 
     @Override
+    @Transactional(rollbackFor = Exception.class)
     public BaseResult<Boolean> doHandle(Long id) {
         //1.验证操作人是否是经办人或其上级,状态是否是bug打开
 
         //2.查找bug数据
 
-        //3.赋值:经办人为本次操作人,上一阶段经办人不变(测试角色),状态变成待修复
+        //3.赋值:经办人不变,上一阶段经办人不变(bug提出人),状态变成待修复
 
         //4.更新bug数据
 
@@ -210,12 +216,13 @@ public class BugOfflineServiceImpl implements BugOfflineService {
     }
 
     @Override
+    @Transactional(rollbackFor = Exception.class)
     public BaseResult<Boolean> passSelf(Long id) {
         //1.验证操作人是否是经办人或其上级,状态是否是待修复
 
         //2.查找bug数据
 
-        //3.赋值:经办人为bug表中上一阶段经办人(测试角色),上一阶段经办人为本次操作人,状态变成待验收
+        //3.赋值:经办人为bug提出人,上一阶段经办人赋值为确认修复时经办人,状态变成待验收
 
         //4.更新bug数据
 
@@ -226,6 +233,7 @@ public class BugOfflineServiceImpl implements BugOfflineService {
     }
 
     @Override
+    @Transactional(rollbackFor = Exception.class)
     public BaseResult<Boolean> accepted(Long id) {
         //1.验证操作人是否是经办人或其上级,状态是否是待验收
 
@@ -240,12 +248,13 @@ public class BugOfflineServiceImpl implements BugOfflineService {
     }
 
     @Override
+    @Transactional(rollbackFor = Exception.class)
     public BaseResult<Boolean> acceptFailed(Long id) {
         //1.验证操作人是否是经办人或其上级,状态是否是待验收
 
         //2.查找bug数据
 
-        //3.赋值:经办人为bug表中上一阶段经办人(自测通过时的数据),上一阶段经办人为本次操作人,状态变成bug打开,bug打回次数+1
+        //3.赋值:经办人为自测通过时上一阶段经办人,上一阶段经办人为本次经办人(经办人与上一阶段经办人交换位置),状态变成bug打开,bug打回次数+1
 
         //4.更新bug数据
 
@@ -254,12 +263,13 @@ public class BugOfflineServiceImpl implements BugOfflineService {
     }
 
     @Override
+    @Transactional(rollbackFor = Exception.class)
     public BaseResult<Boolean> reopen(Long id) {
-        //1.验证操作人是否是提出人或其上级,状态是否是完成或关闭
+        //1.验证操作人是否是提出人或其上级,状态是否是延期修复,完成或关闭
 
         //2.查找bug数据
 
-        //3.赋值:经办人为bug接收人(验收通过或同意时记录的上一阶段操作人),上一阶段经办人为本次操作人,状态变成bug打开
+        //3.赋值:经办人为bug接收人(验收通过或同意时记录的上一阶段操作人),上一阶段经办人为本次经办人(经办人与上一阶段经办人交换位置),状态变成bug打开
 
         //4.完成状态下点击重新打开，bug打回次数+1,完成后重新打开次数+1,更新bug数据
 
@@ -293,6 +303,7 @@ public class BugOfflineServiceImpl implements BugOfflineService {
     }
 
     @Override
+    @Transactional(rollbackFor = Exception.class)
     public BaseResult<Boolean> delete(Long id) {
         return BaseResult.success(true);
     }
