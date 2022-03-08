26a327bcf26ede186669da5b0570f5a6a53da39a
wangxuan <1911491130@qq.com>
2022-03-08 11:40:12 +0800

    优化了被拒绝通知信息

--- model/src/main/java/com/timevale/forward/model/enums/MessageTitleEnum.java
+++ model/src/main/java/com/timevale/forward/model/enums/MessageTitleEnum.java
@@ -24,7 +24,7 @@ public enum MessageTitleEnum {
     BUG_OFFLINE_DELAY_REPAIR("线下bug延期修复通知"),
     BUG_OFFLINE_NO_REPAIR("线下bug不用修复通知"),
     BUG_OFFLINE_TRANS("线下bug转交通知"),
-    BUG_OFFLINE_REJECT("线下bug不用修复被拒绝通知"),
+    BUG_OFFLINE_REJECT("线下bug被拒绝通知"),
     BUG_OFFLINE_OPEN_AGAIN("线下bug重新打开通知")
     ;
 
