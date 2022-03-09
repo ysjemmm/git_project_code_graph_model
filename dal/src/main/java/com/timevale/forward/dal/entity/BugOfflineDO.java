888e13d673aa5efbb694efdc2e206b5757bf244a
wangxuan <1911491130@qq.com>
2022-03-09 11:46:37 +0800

    修复了"不用修复原因之前不能显示"的原因

--- dal/src/main/java/com/timevale/forward/dal/entity/BugOfflineDO.java
+++ dal/src/main/java/com/timevale/forward/dal/entity/BugOfflineDO.java
@@ -115,6 +115,6 @@ public class BugOfflineDO extends BaseDO {
     /**
      * 不用修复原因:0被否定,1重复提交,2无法再次复现,3前端缓存,4产品需求调整,10无
      */
-    private Integer unHandleReason;
+    private Integer unhandleReason;
 
 }
