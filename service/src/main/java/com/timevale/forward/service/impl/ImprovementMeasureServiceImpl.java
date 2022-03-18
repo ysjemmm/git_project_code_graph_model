904f8c8c863d24dcfd50b37260850e5c01e3d495
yangxu <yangxu@tsign.cn>
2022-03-18 18:22:33 +0800

    debug 改回

--- service/src/main/java/com/timevale/forward/service/impl/ImprovementMeasureServiceImpl.java
+++ service/src/main/java/com/timevale/forward/service/impl/ImprovementMeasureServiceImpl.java
@@ -67,8 +67,8 @@ public class ImprovementMeasureServiceImpl implements ImprovementMeasureService 
         // 查看是否创建待办
         if(improvementMeasureDO.getTodo()){
             // 获取 unionId
-            // String userId = LocalSessionUtils.getUserInfo().getId();
-            String userId = "yangxu";
+            String userId = LocalSessionUtils.getUserInfo().getId();
+            // String userId = "yangxu";
             String executorId = improvementMeasureDO.getExecutorId();
             Map<String, String> unionIdMap = innerUserPersonClient.getUnionIds(Lists.newArrayList(userId,executorId));
             if (CollectionUtils.isEmpty(unionIdMap)) {
