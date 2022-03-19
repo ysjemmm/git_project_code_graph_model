aa97e06468a9097c7d037aa8b2b6302eae140e89
yangxu <yangxu@tsign.cn>
2022-03-19 18:37:10 +0800

    fix：数据修改为个人

--- service/src/main/java/com/timevale/forward/service/impl/HomePageServiceImpl.java
+++ service/src/main/java/com/timevale/forward/service/impl/HomePageServiceImpl.java
@@ -98,7 +98,7 @@ public class HomePageServiceImpl implements HomePageService {
         int bizDemandCount = 0;
 
         // 获取我及所有下属
-        List<String> allMyStaffWithSelf = innerUserPersonClient.getAllMyStaffWithSelf(userInfo.getId(), true);
+        List<String> allMyStaffWithSelf = Lists.newArrayList(userInfo.getId());
 
         // 进行中的项目
         List<ProjectDO> projectDOList = projectMapper.selectByTeamMember(allMyStaffWithSelf);
