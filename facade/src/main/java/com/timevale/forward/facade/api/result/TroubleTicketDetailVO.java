6799c6f7f914fe3e040ebd26351e6bf13a252e78
yangxu <yangxu@tsign.cn>
2022-03-29 09:53:32 +0800

    传输过程去除无效字段

--- facade/src/main/java/com/timevale/forward/facade/api/result/TroubleTicketDetailVO.java
+++ facade/src/main/java/com/timevale/forward/facade/api/result/TroubleTicketDetailVO.java
@@ -1,5 +1,6 @@
 package com.timevale.forward.facade.api.result;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import com.timevale.mandarin.common.result.ToString;
@@ -19,6 +20,7 @@ import java.util.List;
 @EqualsAndHashCode(callSuper = true)
 @Data
 @ApiModel("故障工单详情")
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class TroubleTicketDetailVO extends ToString {
 
     @ApiModelProperty("id")
