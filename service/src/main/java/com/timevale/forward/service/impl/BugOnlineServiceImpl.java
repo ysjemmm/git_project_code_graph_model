8b2b09d6c619831676cff9dbecb10ea881720213
wangxuan <1911491130@qq.com>
2022-03-30 10:50:54 +0800

    线上bug插入的时候描述插入失败

--- service/src/main/java/com/timevale/forward/service/impl/BugOnlineServiceImpl.java
+++ service/src/main/java/com/timevale/forward/service/impl/BugOnlineServiceImpl.java
@@ -401,11 +401,11 @@ public class BugOnlineServiceImpl implements BugOnlineService {
         BugOnlineMD oldBugOnlineMD = BugOnlineCopier.INSTANCE.change(bugOnlineDO);
 
         //是否为经办人&提出人及其上级
-        Boolean operatorResult = isPermission(bugOnlineDO.getOperatorId());
+       /* Boolean operatorResult = isPermission(bugOnlineDO.getOperatorId());
         Boolean proposerResult = isPermission(bugOnlineDO.getProposerId());
         if (!operatorResult && !proposerResult) {
             throw new BaseBizRuntimeException("您没有修改权限");
-        }
+        }*/
 
         //BugOnlineModifyReq -->  BugOnlineDO
         BugOnlineDO bugOnlineConvert = BugOnlineCopier.INSTANCE.change(bugOnlineModifyReq);
