3872a6516675dd139dc6142d23e75055a6500967
wangxuan <1911491130@qq.com>
2022-03-30 14:45:52 +0800

    编辑的时候实名主体变化的时候记录一条内容变更数据

--- model/src/main/java/com/timevale/forward/model/middle/BusinessMD.java
+++ model/src/main/java/com/timevale/forward/model/middle/BusinessMD.java
@@ -19,7 +19,7 @@ public class BusinessMD extends BaseMD{
      * 实名主体oid
      */
     @FieldCompare(fieldName = "实名主体oid")
-    private String mainOid;
+    private String mainOId;
 
     /**
      * 模板id
