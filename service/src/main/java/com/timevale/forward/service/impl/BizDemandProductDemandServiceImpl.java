154a684acc1c8b27be3b6e53ffc723de21d55175
yangxu <yangxu@tsign.cn>
2022-04-22 10:08:47 +0800

    fix: merge

--- service/src/main/java/com/timevale/forward/service/impl/BizDemandProductDemandServiceImpl.java
+++ service/src/main/java/com/timevale/forward/service/impl/BizDemandProductDemandServiceImpl.java
@@ -42,7 +42,6 @@ import com.timevale.mandarin.common.annotation.RestService;
 import com.timevale.mandarin.common.result.PageQueryResult;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.collections.CollectionUtils;
 import org.assertj.core.util.Lists;
 import org.springframework.transaction.annotation.Transactional;
 
