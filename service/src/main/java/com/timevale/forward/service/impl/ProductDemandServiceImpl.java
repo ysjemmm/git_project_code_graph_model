5f1a4e972c8718e4e387763f8df55768933effa9
xingyun <xingyun@tsign.cn>
2022-04-24 16:42:15 +0800

    字数限制

--- service/src/main/java/com/timevale/forward/service/impl/ProductDemandServiceImpl.java
+++ service/src/main/java/com/timevale/forward/service/impl/ProductDemandServiceImpl.java
@@ -105,7 +105,7 @@ public class ProductDemandServiceImpl implements ProductDemandService {
     @Resource
     private ProjectLogComponent projectLogComponent;
 
-    private static final Integer MAX_LENGTH = 2 * 1000;
+    private static final Integer MAX_LENGTH = 20 * 1000;
 
 
     @Override
