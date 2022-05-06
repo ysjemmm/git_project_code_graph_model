be9f0284e9aef812a1f4ced2dac869f156d5ba7e
yangxu <yangxu@tsign.cn>
2022-05-06 14:36:03 +0800

    修改更新方法

--- service/src/main/java/com/timevale/forward/service/component/impl/ProjectComponentImpl.java
+++ service/src/main/java/com/timevale/forward/service/component/impl/ProjectComponentImpl.java
@@ -224,7 +224,7 @@ public class ProjectComponentImpl implements ProjectComponent {
         ProjectDO projectDO = new ProjectDO();
         projectDO.setId(projectId);
         projectDO.setNodeStatus(nodeStatus);
-        projectMapper.update(projectDO);
+        projectMapper.updateStatus(projectDO);
     }
 
 
