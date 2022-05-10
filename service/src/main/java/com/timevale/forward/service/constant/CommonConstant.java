1520c50003fcf356107b7dbf87333d5e05e333b2
yangxu <yangxu@tsign.cn>
2022-05-10 15:52:45 +0800

    fix: 排序唯一性

--- service/src/main/java/com/timevale/forward/service/constant/CommonConstant.java
+++ service/src/main/java/com/timevale/forward/service/constant/CommonConstant.java
@@ -8,7 +8,7 @@ public class CommonConstant {
 
     public static final String JOIN_LINE = "-";
 
-    public static final String DEFAULT_ORDER_BY = " modify_date desc ";
+    public static final String DEFAULT_ORDER_BY = " modify_date, id desc ";
 
     public static final Integer INVALID = Integer.MIN_VALUE;
 
