b1df656380b4418643ba2c2d31b9159c30ab46b4
yangxu <yangxu@tsign.cn>
2022-06-02 11:01:49 +0800

    fix:项目风险多选

---
+++ dal/src/main/java/com/timevale/forward/dal/condition/ProjectRiskCondition.java
@@ -0,0 +1,24 @@
+package com.timevale.forward.dal.condition;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * @author by YangXu
+ * @date 2022/06/02 10:49
+ */
+@Data
+@Builder
+public class ProjectRiskCondition{
+    /**
+     * 项目id
+     */
+    private Long projectId;
+
+    /**
+     * 状态列表
+     */
+    private List<Integer> statusList;
+}
