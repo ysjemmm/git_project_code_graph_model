ebec99a5b0a3736b1d86343f168a039821140888
yangxu <yangxu@tsign.cn>
2022-06-08 15:44:39 +0800

    取消关联不变动

--- service/src/main/java/com/timevale/forward/service/impl/BizDemandProductDemandServiceImpl.java
+++ service/src/main/java/com/timevale/forward/service/impl/BizDemandProductDemandServiceImpl.java
@@ -157,7 +157,7 @@ public class BizDemandProductDemandServiceImpl implements BizDemandProductDemand
 
         bizDemandComponent.updateBizDemandStatusByLinkedProductDemand(bizDemandId);
 
-        BizDemandStatusVO bizDemandStatusVO = compareBizDemandStatus(bizDemandDO);
+        BizDemandStatusVO bizDemandStatusVO = compareBizDemandStatus(bizDemandDO, true);
 
         // 日志
         bizDemandLogComponent.addLogWhenBizDemandLinkProductDemand(bizDemandId, productDemandIdList);
@@ -194,7 +194,7 @@ public class BizDemandProductDemandServiceImpl implements BizDemandProductDemand
         bizDemandComponent.updateBizDemandStatusByLinkedProductDemand(bizDemandId);
 
         // 判断当前状态
-        BizDemandStatusVO bizDemandStatusVO = compareBizDemandStatus(bizDemandDO);
+        BizDemandStatusVO bizDemandStatusVO = compareBizDemandStatus(bizDemandDO, false);
 
         // 产品需求关联日志
         bizDemandLogComponent.addLogWhenBizDemandUnLinkProductDemand(bizDemandId, productDemandId);
@@ -202,7 +202,7 @@ public class BizDemandProductDemandServiceImpl implements BizDemandProductDemand
         return BaseResult.success(bizDemandStatusVO);
     }
 
-    private BizDemandStatusVO compareBizDemandStatus(BizDemandDO oldBizDemandDO){
+    private BizDemandStatusVO compareBizDemandStatus(BizDemandDO oldBizDemandDO, Boolean update){
         Long bizDemandId = oldBizDemandDO.getId();
         BizDemandDO newBizDemandDO = bizDemandMapper.selectById(bizDemandId);
 
@@ -253,7 +253,7 @@ public class BizDemandProductDemandServiceImpl implements BizDemandProductDemand
 
 
             // 更新预期上线时间
-            if(newEndDate != null){
+            if(newEndDate != null && update){
                 planReleaseDate = DateUtil.getMonth(newEndDate) - 1;
                 newBizDemandDO.setPlanReleaseDate(planReleaseDate);
 
