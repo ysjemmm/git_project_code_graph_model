e236b6987de8bc0e8de8d4801ab8b1f665763bbc
yangxu <yangxu@tsign.cn>
2022-06-10 16:24:33 +0800

    改进措施-单测

--- deploy/src/test/java/com/timevale/forward/service/impl/ImprovementMeasureServiceImplTest.java
+++ deploy/src/test/java/com/timevale/forward/service/impl/ImprovementMeasureServiceImplTest.java
@@ -2,7 +2,8 @@ package com.timevale.forward.service.impl;
 
 import com.timevale.forward.dal.dao.ImprovementMeasureMapper;
 import com.timevale.forward.dal.entity.ImprovementMeasureDO;
-import com.timevale.forward.facade.api.client.ImprovementMeasureService;
+import com.timevale.forward.facade.api.request.ImprovementMeasureCompleteReq;
+import com.timevale.forward.facade.api.request.ImprovementMeasureDeleteReq;
 import com.timevale.forward.facade.api.request.ImprovementMeasureModifyReq;
 import com.timevale.forward.service.component.ImprovementMeasureComponent;
 import com.timevale.forward.service.integration.erp.DingWorkRecordClient;
@@ -16,6 +17,8 @@ import org.testng.annotations.Test;
 
 import java.util.Collections;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doNothing;
@@ -29,7 +32,7 @@ import static org.mockito.Mockito.when;
 public class ImprovementMeasureServiceImplTest extends AbstractTestNGSpringContextTests {
 
     @InjectMocks
-    private ImprovementMeasureService improvementMeasureService;
+    private ImprovementMeasureServiceImpl improvementMeasureService;
 
     @Mock
     private ImprovementMeasureMapper improvementMeasureMapper;
@@ -55,30 +58,61 @@ public class ImprovementMeasureServiceImplTest extends AbstractTestNGSpringConte
         improvementMeasureDO.setTodo(true);
         improvementMeasureDO.setTodoId("12345");
         improvementMeasureDO.setExecutor("executor");
-        improvementMeasureDO.setExecutor("executorId");
+        improvementMeasureDO.setExecutorId("executorId");
         when(improvementMeasureMapper.selectByCondition(any())).thenReturn(Collections.singletonList(improvementMeasureDO));
 
         doNothing().when(improvementMeasureComponent).updateTodoTask(any());
         doNothing().when(improvementMeasureComponent).deleteTodoTask(any());
-        doNothing().when(improvementMeasureComponent).addTodoTask(any());
 
+        when(improvementMeasureComponent.addTodoTask(any())).thenReturn("1");
         when(improvementMeasureMapper.update(any())).thenReturn(1);
 
         ImprovementMeasureModifyReq req = new ImprovementMeasureModifyReq();
         req.setImplementationTime(new Date());
 
-        req.setTodo(false);
-        assert improvementMeasureService.modify(req).ifSuccess();
-
         req.setTodo(true);
         req.setExecutor("executor");
-        req.setExecutor("executorId");
+        req.setExecutorId("executorId");
         assert improvementMeasureService.modify(req).ifSuccess();
 
         req.setTodo(true);
         req.setExecutor("executor-diff");
-        req.setExecutor("executorId-diff");
+        req.setExecutorId("executorId-diff");
         assert improvementMeasureService.modify(req).ifSuccess();
+
+        improvementMeasureDO.setTodo(false);
+        when(improvementMeasureMapper.selectByCondition(any())).thenReturn(Collections.singletonList(improvementMeasureDO));
+        assert  improvementMeasureService.modify(req).ifSuccess();
+    }
+
+    @Test
+    public void testDelete(){
+        ImprovementMeasureDeleteReq req = new ImprovementMeasureDeleteReq();
+        req.setId(1L);
+        doNothing().when(improvementMeasureComponent).delete(any());
+        assert improvementMeasureService.delete(req).ifSuccess();
+    }
+
+    @Test
+    public void testComplete(){
+        ImprovementMeasureCompleteReq req = new ImprovementMeasureCompleteReq();
+        req.setId(1L);
+
+        ImprovementMeasureDO improvementMeasureDO = new ImprovementMeasureDO();
+        improvementMeasureDO.setName("1");
+        improvementMeasureDO.setTodo(true);
+        improvementMeasureDO.setExecutorId("1");
+        improvementMeasureDO.setImplementationTime(new Date());
+        when(improvementMeasureMapper.selectByCondition(any())).thenReturn(Collections.singletonList(improvementMeasureDO));
+        when(improvementMeasureMapper.update(any())).thenReturn(1);
+
+        Map<String, String> unionIdMap = new HashMap<>();
+        unionIdMap.put("1","1");
+        when(innerUserPersonClient.getUnionIds(any())).thenReturn(unionIdMap);
+
+        doNothing().when(dingWorkRecordClient).updateTask(any());
+
+        assert improvementMeasureService.complete(req).ifSuccess();
     }
 
 }
