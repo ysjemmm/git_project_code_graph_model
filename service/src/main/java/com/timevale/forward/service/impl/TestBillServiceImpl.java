fd2ae4cdbe02f846465361bc3f6ab2b12b3a189a
yangxu <yangxu@tsign.cn>
2022-06-16 14:07:59 +0800

    fix: 项目节点状态

--- service/src/main/java/com/timevale/forward/service/impl/TestBillServiceImpl.java
+++ service/src/main/java/com/timevale/forward/service/impl/TestBillServiceImpl.java
@@ -414,9 +414,6 @@ public class TestBillServiceImpl implements TestBillService {
         //更新项目节点表
         projectNodeMapper.updateSubmitTestActualDate(testBillModifyReq.getProjectId(), testBillModifyReq.getActualDate());
 
-        // 更新项目节点状态
-        projectComponent.updateNodeStatus(testBillModifyReq.getProjectId());
-
         Integer oldStatus = projectDO.getStatus();
         Integer newStatus = projectComponent.getStatus(projectDO.getId());
         if (!ProjectStatusEnum.INVALID.getCode().equals(oldStatus)
@@ -438,6 +435,9 @@ public class TestBillServiceImpl implements TestBillService {
                         testBillModifyReq.getProjectId()
                 )
         );
+
+        // 更新项目节点状态
+        projectComponent.updateNodeStatus(testBillModifyReq.getProjectId());
 
         return BaseResult.success(true);
     }
