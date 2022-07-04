47921ba7fe249d3ccc645646c6d87e8cbcad88dd
jingchun <jingchun@tsign.cn>
2022-07-04 11:19:20 +0800

    feat: 添加需求变更请求体

---
+++ model/src/main/java/com/timevale/forward/model/enums/ProductDemandDescChangeTypeEnum.java
@@ -0,0 +1,22 @@
+package com.timevale.forward.model.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * @author jingchun
+ * create on 2022/7/4
+ */
+@Getter
+@AllArgsConstructor
+public enum ProductDemandDescChangeTypeEnum {
+    INSUFFICIENT_SURVEY(0, "需求调研不充分"),
+    DEMAND_CHANGE(1, "需求变更"),
+    MISAPPREHENSION(2, "理解偏差"),
+    BROKEN_FLOW(3, "需求对现有流程造成改动"),
+    ELSE(9, "其他")
+    ;
+    private final Integer code;
+    private final String text;
+
+}
