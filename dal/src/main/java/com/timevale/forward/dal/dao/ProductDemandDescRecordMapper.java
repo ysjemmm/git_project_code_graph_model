8a5550b00cf2ee49577c6865290a2426d72580a2
jingchun <jingchun@tsign.cn>
2022-07-04 15:29:17 +0800

    feat: 添加返回变更次数，fix typos

--- dal/src/main/java/com/timevale/forward/dal/dao/ProductDemandDescRecordMapper.java
+++ dal/src/main/java/com/timevale/forward/dal/dao/ProductDemandDescRecordMapper.java
@@ -14,6 +14,10 @@ import java.util.List;
 public interface ProductDemandDescRecordMapper {
 
     List<ProductDemandDescRecordDO> listByProductDemandId(@Param("productDemandId") Long productDemandId);
+
+
+    Integer countByProductDemandId(@Param("productDemandId") Long productDemandId);
+
 }
 
 
