08fc2f190a04a3ee1258264279276a945ec96add
jingchun <jingchun@tsign.cn>
2022-07-06 18:26:49 +0800

    fix: 修复人天搜索结果为空时项目总人天错误

--- service/src/main/java/com/timevale/forward/service/impl/ManDayServiceImpl.java
+++ service/src/main/java/com/timevale/forward/service/impl/ManDayServiceImpl.java
@@ -212,7 +212,6 @@ public class ManDayServiceImpl implements ManDayService {
         // 项目总人天总是返回
         res.setProjectActualManDay(manDayMapper.sumProjectActualDays(projectId));
         if (manDays.isEmpty()) {
-            res.setProjectActualManDay(BigDecimal.ZERO);
             res.setProjectManDays(Collections.emptyList());
             return BaseResult.success(res);
         }
