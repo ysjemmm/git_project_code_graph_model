2595afee80a8fa32fd501a8fffb6ba7694c73353
jingchun <jingchun@tsign.cn>
2022-07-11 18:41:10 +0800

    fix: 添加排序相关字段

--- facade/src/main/java/com/timevale/forward/facade/api/query/ProjectIdPageQuery.java
+++ facade/src/main/java/com/timevale/forward/facade/api/query/ProjectIdPageQuery.java
@@ -1,6 +1,7 @@
 package com.timevale.forward.facade.api.query;
 
 import com.timevale.mandarin.common.query.QueryBase;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,5 +13,11 @@ public class ProjectIdPageQuery extends QueryBase {
 
     @NotNull(message = "项目id不能为空")
     private Long projectId;
+
+    @ApiModelProperty("排序字段")
+    private String orderFiled;
+
+    @ApiModelProperty("排序规则：0正序，1逆序")
+    private Integer orderCollation;
 
 }
