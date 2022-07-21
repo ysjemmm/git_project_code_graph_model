b09134fe305fbc89fb2dd58b6f6407bf37d0fcf8
xingyun <xingyun@tsign.cn>
2022-07-21 15:27:46 +0800

    待UED评审节点 兼容

--- model/src/main/java/com/timevale/forward/model/enums/ProjectNodeEnum.java
+++ model/src/main/java/com/timevale/forward/model/enums/ProjectNodeEnum.java
@@ -22,6 +22,7 @@ public enum ProjectNodeEnum {
     DEMAND_INTERNAL_AUDIT(10,"需求内审"),
     DEMAND_CONSTRUE(20,"需求串讲"),
     DEMAND_CONSTRUE_REVERSE(25,"需求反串讲"),
+    READY_UED_AUDIT(27,"待UED评审"),
     TECHNICAL_DETAIL_REVIEW(30,"技术详设评审"),
     DEVELOP_START(40,"开发开始"),
     WRITE_TEST_CASES(50,"编写测试用例"),
