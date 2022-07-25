366c79567e95ada4e7752c1f6fb2d457ebee9918
xingyun <xingyun@tsign.cn>
2022-07-25 16:47:45 +0800

    log

--- facade/src/main/java/com/timevale/forward/facade/api/request/ProjectModifyReq.java
+++ facade/src/main/java/com/timevale/forward/facade/api/request/ProjectModifyReq.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import javax.validation.constraints.NotNull;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -31,5 +32,11 @@ public class ProjectModifyReq extends ProjectAddReq {
     @ApiModelProperty("延期类型:0提测延期,1发布正式延期,-1不延期")
     @NotNull(message = "延期类型不能为空")
     private Integer delayType;
+
+    @ApiModelProperty("立项开始时间")
+    private Date pjEstablishStartDate;
+
+    @ApiModelProperty("立项预期上线时间")
+    private Date pjEstablishPublishDate;
 
 }
