552114e9d39a1dededb27051c34db52b99f6a8a6
yangxu <yangxu@tsign.cn>
2022-08-01 13:50:18 +0800

    feat: 项目目标，业务域仅=数智化，逻辑

--- service/src/main/java/com/timevale/forward/service/job/ProjectGoalReachDateNotifyJob.java
+++ service/src/main/java/com/timevale/forward/service/job/ProjectGoalReachDateNotifyJob.java
@@ -1,10 +1,8 @@
 package com.timevale.forward.service.job;
 
 import com.google.common.collect.Maps;
-import com.timevale.forward.dal.dao.ProjectGoalMapper;
-import com.timevale.forward.dal.dao.ProjectMapper;
-import com.timevale.forward.dal.entity.ProjectDO;
-import com.timevale.forward.dal.entity.ProjectGoalDO;
+import com.timevale.forward.dal.dao.*;
+import com.timevale.forward.dal.entity.*;
 import com.timevale.forward.model.enums.ProjectGoalStatusEnum;
 import com.timevale.forward.model.enums.YesOrNoEnum;
 import com.timevale.forward.service.integration.erp.ErpMessageClient;
@@ -12,7 +10,9 @@ import com.timevale.forward.service.integration.erp.model.MarkdownMsg;
 import com.timevale.framework.schedulerT.client.annotaion.JobHandler;
 import com.timevale.framework.schedulerT.core.biz.model.ReturnT;
 import com.timevale.framework.schedulerT.core.handler.IJobHandler;
+import com.timevale.mandarin.base.util.CollectionUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.util.Sets;
 import org.springframework.beans.factory.annotation.Value;
 
 import javax.annotation.Resource;
@@ -32,6 +32,8 @@ public class ProjectGoalReachDateNotifyJob extends IJobHandler {
 
     @Value("${projectGoal.receiver:zhuque}")
     private String reachGoalReceiver;
+    // 不通知的业务域
+    private final String excludeBizDomain = "数智化中心";
 
     @Resource
     private ProjectGoalMapper projectGoalMapper;
@@ -39,6 +41,12 @@ public class ProjectGoalReachDateNotifyJob extends IJobHandler {
     private ProjectMapper projectMapper;
     @Resource
     private ErpMessageClient erpMessageClient;
+    @Resource
+    private ProductLineMapper productLineMapper;
+    @Resource
+    private BizDomainMapper bizDomainMapper;
+    @Resource
+    private ProjectProductLineMapper projectProductLineMapper;
 
     @Override
     public ReturnT<String> execute(String s) {
@@ -47,9 +55,33 @@ public class ProjectGoalReachDateNotifyJob extends IJobHandler {
         Set<Long> projectIds = projectGoals.stream().map(ProjectGoalDO::getProjectId)
                 .collect(Collectors.toSet());
         List<ProjectDO> projects = projectMapper.getByIds(projectIds);
+
+        // 项目关联产品线
+        List<ProjectProductLineDO> projectProductLineDOList = projectProductLineMapper.getByProjectIdList(new ArrayList<>(projectIds));
+        Map<Long, Set<Long>> projectProductLineGroup = new HashMap<>();
+        for (ProjectProductLineDO e : projectProductLineDOList) {
+            Long projectId = e.getProjectId();
+            Long productLineId = e.getProductLineId();
+
+            Set<Long> productLineIdSet = projectProductLineGroup.get(projectId);
+            if (CollectionUtils.isEmpty(productLineIdSet)) {
+                productLineIdSet = new HashSet<>();
+            }
+            productLineIdSet.add(productLineId);
+            projectProductLineGroup.put(projectId, productLineIdSet);
+        }
+
+        // 产品线、业务域信息
+        List<Long> productLineIdList = projectProductLineDOList.stream().map(ProjectProductLineDO::getProductLineId).collect(Collectors.toList());
+        List<ProductLineDO> productLineDOList = productLineMapper.selectByIds(productLineIdList);
+        List<Long> bizDomainIdList = productLineDOList.stream().map(ProductLineDO::getBizDomainId).collect(Collectors.toList());
+        List<BizDomainDO> bizDomainDOList = bizDomainMapper.selectByIdList(bizDomainIdList);
+        Map<Long, String> bizDomainMap = bizDomainDOList.stream().collect(Collectors.toMap(BaseDO::getId, BizDomainDO::getName));
+
         Map<Long, ProjectDO> projectById = Maps.uniqueIndex(projects, ProjectDO::getId);
         for (ProjectGoalDO projectGoal : projectGoals) {
             ProjectDO project = projectById.get(projectGoal.getProjectId());
+
             if (project == null || YesOrNoEnum.NO.getCode().equals(project.getIsWithGoal())) {
                 // 无项目或者无项目目标，过滤
                 log.info("项目无项目目标，过滤目标: {}", projectGoal);
@@ -66,6 +98,15 @@ public class ProjectGoalReachDateNotifyJob extends IJobHandler {
                 continue;
             }
             log.info("通知项目到期: {}", projectGoal);
+
+            // 如果业务域仅=数智化中，则不通知
+            Long projectId = project.getId();
+            Set<Long> productLineIdSet = projectProductLineGroup.get(projectId);
+            Set<String> bizDomainSet = productLineIdSet.stream().map(bizDomainMap::get).collect(Collectors.toSet());
+            if(bizDomainSet.size() == 1 && bizDomainSet.contains(excludeBizDomain)) {
+                continue;
+            }
+
             erpMessageClient.sendMarkdownMsg(MarkdownMsg.builder()
                     .title(TITLE)
                     .content(String.format(NOTIFY_PATTERN, project.getName()))
