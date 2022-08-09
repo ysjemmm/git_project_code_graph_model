8d667ab12366fbb5f34e579c9bbd9e66f4cc81ff
yangxu <yangxu@tsign.cn>
2022-08-09 17:50:46 +0800

    chore: 去除权限校验

--- service/src/main/java/com/timevale/forward/service/impl/ProjectGoalServiceImpl.java
+++ service/src/main/java/com/timevale/forward/service/impl/ProjectGoalServiceImpl.java
@@ -262,7 +262,6 @@ public class ProjectGoalServiceImpl implements ProjectGoalService {
         AssertUtil.checkState(projectGoalFinishReq.getStatus().equals(ProjectGoalStatusEnum.FINISHED.getCode()) ||
                         projectGoalFinishReq.getStatus().equals(ProjectGoalStatusEnum.UNFINISHED.getCode()),
                 "完成状态只能是 10-已完成 或 30-未完成");
-        AssertUtil.checkState(hasGoalFinishPermission(), "您没有该操作权限");
         AssertUtil.checkState(ProjectGoalStatusEnum.IN_PROGRESS.getCode().equals(goal.getStatus()),
                 "目标只有在进行中时可以填写完成情况");
         // 更新目标
