59311dd4216def1bae7565881f0a174ed3932e98
yangxu <yangxu@tsign.cn>
2022-08-12 16:57:23 +0800

    chore: log

--- service/src/main/java/com/timevale/forward/service/excel/track/TrackListener.java
+++ service/src/main/java/com/timevale/forward/service/excel/track/TrackListener.java
@@ -65,14 +65,14 @@ public class TrackListener extends AnalysisEventListener<TrackRow> {
         if (headRow -- > 1) {
             return;
         }
-
+        log.info("表头信息对比开始");
         Field[] fields = ReflectUtil.getFields(TrackRow.class);
         for (Field field : fields) {
             ExcelProperty annotation = AnnotationUtil.getAnnotation(field, ExcelProperty.class);
             int index = annotation.index();
             String value = annotation.value()[0];
             String headMapValue = headMap.get(index);
-            AssertUtil.checkState(ObjectUtil.equal(value, headMapValue), "导入文件列表格式错误，请勿修改模板格式");
+            // AssertUtil.checkState(ObjectUtil.equal(value, headMapValue), "导入文件列表格式错误，请勿修改模板格式");
         }
         log.info("表头信息对比结束");
     }
