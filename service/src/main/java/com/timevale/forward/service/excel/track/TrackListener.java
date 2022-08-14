8fa59f3ef2f9c0bd0778bb0b0b961e6ebdcea415
cai <850159901@qq.com>
2022-08-15 03:53:01 +0800

    feat: 模板校验

--- service/src/main/java/com/timevale/forward/service/excel/track/TrackListener.java
+++ service/src/main/java/com/timevale/forward/service/excel/track/TrackListener.java
@@ -1,12 +1,19 @@
 package com.timevale.forward.service.excel.track;
 
+import cn.hutool.core.annotation.AnnotationUtil;
 import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.util.ObjectUtil;
+import cn.hutool.core.util.ReflectUtil;
+import com.alibaba.excel.annotation.ExcelProperty;
 import com.alibaba.excel.context.AnalysisContext;
 import com.alibaba.excel.event.AnalysisEventListener;
 import com.timevale.forward.service.copy.TrackPropCopier;
+import com.timevale.mandarin.base.util.AssertUtil;
 import lombok.extern.slf4j.Slf4j;
 
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author by YangXu
@@ -16,7 +23,7 @@ import java.util.List;
 public class TrackListener extends AnalysisEventListener<TrackRow> {
 
     private int eventIndex = 0;
-    private int headRow = 3;
+    private int headRow = 2;
     private final List<TrackEvent> trackEventList;
 
     public TrackListener(List<TrackEvent> trackEventList) {
@@ -51,5 +58,23 @@ public class TrackListener extends AnalysisEventListener<TrackRow> {
     @Override
     public void doAfterAllAnalysed(AnalysisContext analysisContext) {
         log.info("导入文件读取完成");
+    }
+
+    public void invokeHeadMap(Map<Integer, String> headMap, AnalysisContext context) {
+        if (headRow > 0) {
+            headRow--;
+            return;
+        }
+        log.info("表头信息对比开始");
+        Field[] fields = ReflectUtil.getFields(TrackRow.class);
+        for (int i = 0; i < 16; i++) {
+            Field field = fields[i];
+            ExcelProperty annotation = AnnotationUtil.getAnnotation(field, ExcelProperty.class);
+            int index = annotation.index();
+            String value = annotation.value()[0];
+            String headMapValue = headMap.get(index);
+            AssertUtil.checkState(ObjectUtil.equal(value, headMapValue), "导入文件列表格式错误，请勿修改模板格式");
+        }
+        log.info("表头信息对比结束");
     }
 }
