a88a494648acf987bb605ccedc6eafafae4d62e0
yangxu <yangxu@tsign.cn>
2022-08-23 10:48:36 +0800

    chore: 修改判断sql

--- service/src/main/java/com/timevale/forward/service/impl/TroubleTicketServiceImpl.java
+++ service/src/main/java/com/timevale/forward/service/impl/TroubleTicketServiceImpl.java
@@ -244,7 +244,7 @@ public class TroubleTicketServiceImpl implements TroubleTicketService {
         String orderFiled = troubleTicketQueryList.getOrderFiled();
         Integer orderCollation = troubleTicketQueryList.getOrderCollation();
         if (Objects.equals(orderFiled, "troubleRank")) {
-            collation = "IF(trouble_rank IS NULL, 200, IF(trouble_rank < 0, -10 * trouble_rank, trouble_rank))";
+            collation = "FIELD(COALESCE(trouble_rank, -20), 0, 10, 20, 30, 40, 50, -10, -20)";
             if (Objects.equals(OrderCollationEnum.DESC.getCode(), orderCollation)) {
                 collation += "desc";
             }
