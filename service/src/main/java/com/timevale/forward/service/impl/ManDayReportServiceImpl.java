9bc27a799e25bef972971dd022525eb5b53fcfa5
yangxu <yangxu@tsign.cn>
2022-08-24 10:52:55 +0800

    chore: 提报排序

--- service/src/main/java/com/timevale/forward/service/impl/ManDayReportServiceImpl.java
+++ service/src/main/java/com/timevale/forward/service/impl/ManDayReportServiceImpl.java
@@ -85,7 +85,7 @@ public class ManDayReportServiceImpl implements ManDayReportService {
         condition.setWeekEndDate(dateDatePair.getRight());
 
         PageHelper.startPage(manDayReportQueryList.getPageNum(), manDayReportQueryList.getPageSize(),
-                "mdr.audit_status desc, mdr.modify_date desc, mdr.id desc");
+                "mdr.audit_status desc, mdr.create_date desc, mdr.id desc");
         List<ManDayReportListDO> reportDOList = manDayReportMapper.selectCondition(condition);
         List<ManDayReportListVO> reportVOList = reportDOList.stream().map(ManDayReportCopier.INSTANCE::convert).collect(Collectors.toList());
 
