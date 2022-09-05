aed6320637d296d29b541c600d3600bbdfc7f325
xiaoyun <xiaoyun@tsign.cn>
2022-09-05 16:34:05 +0800

    【产研系统-用户调研-9月优化上】定时任务fix

--- service/src/main/java/com/timevale/forward/service/utils/date/DateUtil.java
+++ service/src/main/java/com/timevale/forward/service/utils/date/DateUtil.java
@@ -2038,8 +2038,25 @@ public class DateUtil implements DateFormatConst {
         return getShotDate(dayBefore);
     }
 
+    public static Date getThisWeekMonday(Date date) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        // 获得当前日期是一个星期的第几天
+        int dayWeek = cal.get(Calendar.DAY_OF_WEEK);
+        if (1 == dayWeek) {
+            cal.add(Calendar.DAY_OF_MONTH, -1);
+        }
+        // 设置一个星期的第一天，按中国的习惯一个星期的第一天是星期一
+        cal.setFirstDayOfWeek(Calendar.MONDAY);
+        // 获得当前日期是一个星期的第几天
+        int day = cal.get(Calendar.DAY_OF_WEEK);
+        // 根据日历的规则，给当前日期减去星期几与一个星期第一天的差值
+        cal.add(Calendar.DATE, cal.getFirstDayOfWeek() - day);
+        return cal.getTime();
+    }
+
     public static String getLastSunDay() {
-        Date monday = getMondayOfWeek(new Date());
+        Date monday = getThisWeekMonday(new Date());
 
         return getDayBeforeStr(monday, -1);
 
