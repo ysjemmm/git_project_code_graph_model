c6b01141442f7c05d392e053f257a7b06af65f7f
xingyun <xingyun@tsign.cn>
2022-09-15 10:26:46 +0800

    客开2期

--- facade/src/main/java/com/timevale/forward/facade/api/result/BizDemandSimpleVO.java
+++ facade/src/main/java/com/timevale/forward/facade/api/result/BizDemandSimpleVO.java
@@ -11,7 +11,7 @@ import lombok.EqualsAndHashCode;
 import java.util.Date;
 
 /**
- * @author by YangXu
+ * @author by xingyun
  * @date 2021/12/14 15:52
  */
 @Data
@@ -28,6 +28,12 @@ public class BizDemandSimpleVO extends ToString {
 
     @ApiModelProperty("需求解决状态:0待评估，10已接收，13待确认，15已完成无需开发，17已关联产品需求，20已列入项目，30项目进行中，40已完成上线，-10被驳回，-20已作废")
     private Integer status;
+
+    @ApiModelProperty("优先级：0-紧急，10-高，20-中，30低")
+    private Integer priority;
+
+    @ApiModelProperty("预计上线时间:0~11 分别对应1~12月,20:暂时无法评估")
+    private Integer planReleaseDate;
 
     @ApiModelProperty("创建时间")
     private Date createDate;
