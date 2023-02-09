d6ca5d52ae3ab75dc830b18a2db7b8bfa455fe18
jingchun <jingchun@tsign.cn>
2023-02-09 13:31:02 +0800

    fix: 修复executor无用户姓名的问题

--- service/src/main/java/com/timevale/forward/service/impl/HomePageServiceImpl.java
+++ service/src/main/java/com/timevale/forward/service/impl/HomePageServiceImpl.java
@@ -587,7 +587,9 @@ public class HomePageServiceImpl implements HomePageService {
         }
         users.removeIf(u -> !userIds.contains(u.getAccount()));
         List<HomePageGroupWorkTimeVO> res = users.stream().map(u ->
-                new HomePageGroupWorkTimeVO(u.getAccount(), u.getAlias(), new ArrayList<>())
+                new HomePageGroupWorkTimeVO(u.getAccount(),
+                        userInfo.getAlias() + CommonConstant.JOIN_LINE + u.getName(),
+                        new ArrayList<>())
         ).collect(Collectors.toList());
         List<TaskBoardDTO> tasks = listTasksSuitDateRange(req.getStartDate(), req.getEndDate(), userIds);
         if (tasks.isEmpty()) {
