e5b04bfc021ae614a71de8038792cfbf94d3d104
yangxu <yangxu@tsign.cn>
2023-03-03 16:44:27 +0800

    feat: 项目评价接口实现

---
+++ service/src/main/java/com/timevale/forward/service/impl/ProjectEvaluateServiceImpl.java
@@ -0,0 +1,34 @@
+package com.timevale.forward.service.impl;
+
+import com.timevale.footstone.base.model.response.BaseResult;
+import com.timevale.forward.facade.api.client.ProjectEvaluateService;
+import com.timevale.forward.facade.api.request.MemberEvaluateModifyReq;
+import com.timevale.forward.facade.api.result.ProjectMemberEvaluateVO;
+import com.timevale.forward.service.utils.aop.LogPoint;
+import com.timevale.mandarin.common.annotation.RestService;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@LogPoint
+@RestService
+public class ProjectEvaluateServiceImpl implements ProjectEvaluateService {
+    @Override
+    public BaseResult<ProjectMemberEvaluateVO> memberList(Long projectId) {
+        return null;
+    }
+
+    @Override
+    public BaseResult<Boolean> updateMember(MemberEvaluateModifyReq req) {
+        return null;
+    }
+
+    @Override
+    public BaseResult<ProjectMemberEvaluateVO> list(Long projectId) {
+        return null;
+    }
+
+    @Override
+    public BaseResult<Boolean> update(MemberEvaluateModifyReq req) {
+        return null;
+    }
+}
