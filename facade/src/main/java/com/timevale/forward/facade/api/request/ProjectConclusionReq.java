b1b73daceb97460f3940fc2db625abd48934cc2f
yangxu <yangxu@tsign.cn>
2023-03-10 18:48:58 +0800

    fix: 项目评价修改问题

---
+++ facade/src/main/java/com/timevale/forward/facade/api/request/ProjectConclusionReq.java
@@ -0,0 +1,27 @@
+package com.timevale.forward.facade.api.request;
+
+import com.timevale.mandarin.common.result.ToString;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotNull;
+
+
+/**
+ * @author by YangXu
+ * @date 2023/02/10 16:38
+ */
+@Getter
+@Setter
+@ApiModel("项目结项请求")
+public class ProjectConclusionReq extends ToString {
+    @NotNull(message = "项目id必填")
+    @ApiModelProperty("项目id")
+    private Long projectId;
+
+    @NotNull(message = "项目最终状态必填: ")
+    @ApiModelProperty("项目最终状态")
+    private Integer targetStatus;
+}
