9a488f8364af53c7d82f384d72c5910aedb6e837
jingchun <jingchun@tsign.cn>
2023-04-20 17:02:45 +0800

    fix: 修复业务需求期望上线时间变更无变更记录的问题

--- model/src/main/java/com/timevale/forward/model/middle/BizDemandMD.java
+++ model/src/main/java/com/timevale/forward/model/middle/BizDemandMD.java
@@ -7,6 +7,8 @@ import com.timevale.forward.model.enums.PlanReleaseDateEnum;
 import com.timevale.forward.model.enums.YesOrNoEnum;
 import lombok.Data;
 
+import java.util.Date;
+
 
 /**
  * @author yangxu
@@ -62,5 +64,8 @@ public class BizDemandMD extends BaseMD {
 
     @FieldCompare(fieldName = "产品方案")
     private String productSolution;
+
+    @FieldCompare(fieldName = "期望上线时间")
+    private Date hopeReleaseDay;
 
 }
