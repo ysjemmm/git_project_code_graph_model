6660669beafd56807eedc1574a42253f69d4b03a
yangxu <yangxu@tsign.cn>
2023-06-13 18:33:30 +0800

    fix: enum code conflict

--- model/src/main/java/com/timevale/forward/model/enums/ProblemOccurredTimeEnum.java
+++ model/src/main/java/com/timevale/forward/model/enums/ProblemOccurredTimeEnum.java
@@ -12,9 +12,9 @@ import java.util.Optional;
 @Getter
 @AllArgsConstructor
 public enum ProblemOccurredTimeEnum {
-    WITHIN_A_DAY(0, "24小时以内", 0),
-    WITHIN_THREE_DAYS(0, "24~72小时", 5),
-    MORE_THAN_THREE_DAYS(0, "72小时以上", 10);
+    WITHIN_A_DAY(1, "24小时以内", 0),
+    WITHIN_THREE_DAYS(2, "24~72小时", 5),
+    MORE_THAN_THREE_DAYS(3, "72小时以上", 10);
 
     private final Integer code;
     private final String text;
