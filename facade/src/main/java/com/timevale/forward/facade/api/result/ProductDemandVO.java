f56ecc2e4e9d8dabd7c6951ddc442c0abb075d1b
jingchun <jingchun@tsign.cn>
2023-08-03 17:44:59 +0800

    doc: 补充枚举说明

--- facade/src/main/java/com/timevale/forward/facade/api/result/ProductDemandVO.java
+++ facade/src/main/java/com/timevale/forward/facade/api/result/ProductDemandVO.java
@@ -42,10 +42,10 @@ public class ProductDemandVO extends ToString {
     @ApiModelProperty("类型:0新增功能,1功能迭代,2体验优化,3技术需求,4安全需求")
     private Integer type;
 
-    @ApiModelProperty("状态:0待排期,10已列入项目,20项目进行中,30已完成上线,-10已暂停,-20已作废")
+    @ApiModelProperty("状态:0待排期,5项目已暂停,10已列入项目,20项目进行中,30已完成上线,-10已暂停,-20已作废")
     private Integer status;
 
-    @ApiModelProperty("状态:0待排期,10已列入项目,20项目进行中,30已完成上线,-10已暂停,-20已作废")
+    @ApiModelProperty("状态:0待排期,5项目已暂停,10已列入项目,20项目进行中,30已完成上线,-10已暂停,-20已作废")
     private String statusName;
 
     @ApiModelProperty("负责人")
