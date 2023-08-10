889f45e3c611d873644b54946ea0d1a46742c8a8
jingchun <jingchun@tsign.cn>
2023-08-10 17:23:09 +0800

    fix: 修复发布时间未校验的问题

--- service/src/main/java/com/timevale/forward/service/component/impl/ProjectComponentImpl.java
+++ service/src/main/java/com/timevale/forward/service/component/impl/ProjectComponentImpl.java
@@ -785,6 +785,7 @@ public class ProjectComponentImpl implements ProjectComponent {
         }
         // 计算项目状态
         ProjectNodeDO publishNode = nodeMap.get(ProjectNodeEnum.PUBLISH_OFFICIAL.getText());
+        ProjectNodeDO startNode = nodeMap.get(ProjectNodeEnum.START_PLAN.getText());
         boolean released = publishNode != null && publishNode.getActualDate() != null;
         if (released) {
             if (ProjectStatusEnum.SUSPEND.getCode().equals(oldProject.getStatus())) {
@@ -798,6 +799,10 @@ public class ProjectComponentImpl implements ProjectComponent {
             } else {
                 dataHandler.accept(new ModifyProjectCheckDTO(ModifyCheckTypeEnum.ANY,
                         "项目节点的实际时间已全部填入，状态将变为已发布，已发布的项目不可再编辑。"));
+            }
+            if (publishNode.getActualDate().compareTo(startNode.getActualDate()) < 0) {
+                dataHandler.accept(new ModifyProjectCheckDTO(ModifyCheckTypeEnum.ANY, true,
+                        "项目实际发布时间不能早于实际开始时间"));
             }
         }
 
