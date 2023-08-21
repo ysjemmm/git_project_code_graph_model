3480fea601a9dbbf6485e235e29a609a8c9fc240
shaoye <shaoye@tsign.cn>
2023-08-21 15:39:17 +0800

    feat: bug状态为待验收时经办人自动转为提交人

--- service/src/main/java/com/timevale/forward/service/impl/BugOnlineServiceImpl.java
+++ service/src/main/java/com/timevale/forward/service/impl/BugOnlineServiceImpl.java
@@ -943,6 +943,9 @@ public class BugOnlineServiceImpl implements BugOnlineService {
         bugOnlineDO.setReasonStage(onlineReq.getReasonStage());
         bugOnlineDO.setBugOfflineId(onlineReq.getBugOfflineId());
         bugOnlineDO.setStatus(BugOnlineStatusEnum.ACCEPTANCE.getCode());
+        //bug状态为待验收时经办人自动转为提交人
+        bugOnlineDO.setOperatorId(bugOnlineDO.getProposerId());
+        bugOnlineDO.setOperator(bugOnlineDO.getProposerId());
         bugOnlineMapper.update(bugOnlineDO);
 
         BugLogDO bugLogDO = new BugLogDO();
