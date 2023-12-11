ea0e71b00c1e71ce9e546b9c29f76fb720150141
yangxu <yangxu@tsign.cn>
2023-12-11 19:07:59 +0800

    fix: 清空时未处理固定数据的问题

--- model/src/main/java/com/timevale/forward/model/middle/BugOnlineMD.java
+++ model/src/main/java/com/timevale/forward/model/middle/BugOnlineMD.java
@@ -151,14 +151,10 @@ public class BugOnlineMD extends BaseMD{
     @FieldCompare(fieldName = "用户临时解决方案")
     private String temporarySolution;
 
-
-    /**
-     * 用户临时解决方案
-     */
     @FieldCompare(fieldName = "用户数", enumClass = UserCountEnum.class)
-    private String userCount;
+    private Integer userCount;
 
 
     @FieldCompare(fieldName = "问题发生时长", enumClass = ProblemOccurredTimeEnum.class)
-    private String problemOccurredTime;
+    private Integer problemOccurredTime;
 }
