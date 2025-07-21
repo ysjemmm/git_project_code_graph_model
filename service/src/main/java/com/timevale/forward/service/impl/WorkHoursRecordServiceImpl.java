c847f8826114546a54b42750e3e6b3e6663b0731
yuhua <yuhua@tsign.cn>
2025-07-21 15:22:42 +0800

    feat: 任务删除修改
    提交人：雨桦

--- service/src/main/java/com/timevale/forward/service/impl/WorkHoursRecordServiceImpl.java
+++ service/src/main/java/com/timevale/forward/service/impl/WorkHoursRecordServiceImpl.java
@@ -150,7 +150,7 @@ public class WorkHoursRecordServiceImpl implements WorkHoursRecordService {
             throw new BaseBizRuntimeException("不存在对应的工时记录");
         }
         // 只有任务执行人可删除
-        List<PersonDO> personDOList = personComponent.select(workHoursRecordId, PersonTypeEnum.TASK_EXECUTOR.getCode());
+        List<PersonDO> personDOList = personComponent.select(workHoursRecordDO.getWorkItemId(), PersonTypeEnum.TASK_EXECUTOR.getCode());
         List<String> executorIds = PersonCopier.INSTANCE.transform(personDOList).stream().map(PersonVO::getUserId).collect(Collectors.toList());
         if (!executorIds.contains(workHoursRecordDO.getCreateManId())) {
             throw new BaseBizRuntimeException("非任务执行人不能删除该任务工时");
