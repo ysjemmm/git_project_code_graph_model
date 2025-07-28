8f6d546c4c9082ee7ceaffb8b778957dca65576d
yuhua <yuhua@tsign.cn>
2025-07-28 16:36:42 +0800

    feat: 工时登记免登录实现，修改路径
    提交人：雨桦

--- service/src/main/java/com/timevale/forward/service/controller/NoLoginController.java
+++ service/src/main/java/com/timevale/forward/service/controller/NoLoginController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
@@ -21,6 +22,7 @@ import javax.annotation.Resource;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/workHours")
 public class NoLoginController {
 
     @Resource
--- service/src/main/java/com/timevale/forward/service/job/SendWorkHoursSubmitJob.java
+++ service/src/main/java/com/timevale/forward/service/job/SendWorkHoursSubmitJob.java
@@ -159,7 +159,7 @@ public class SendWorkHoursSubmitJob extends IJobHandler {
             // 生成短链接
             String shortUrl = UrlGenerateUtil.getUrl(userId, token, urlBuilder, dateStr);
             // 完整短链接
-            String fullUrl = url + "/forward/" +shortUrl;
+            String fullUrl = url + "/forward/workHours/" +shortUrl;
 
             // Markdown 内容包含提示
             StringBuilder stringBuilder = new StringBuilder();
