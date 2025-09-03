28197feb4b11acb9d5912aba0f90b6c756ed99bc
yuhua <yuhua@tsign.cn>
2025-09-03 16:44:56 +0800

    feat[产品需求]：产品需求增加抄送人返回
    提交人：雨桦

--- service/src/main/java/com/timevale/forward/service/impl/ProjectServiceImpl.java
+++ service/src/main/java/com/timevale/forward/service/impl/ProjectServiceImpl.java
@@ -1407,6 +1407,11 @@ public class ProjectServiceImpl implements ProjectService {
         log.info("项目-产品需求清单:{},{},{}", pageNum, pageSize, projectId);
         // 查询产品需求
         List<ProductDemandListDO> productDemandListDO = productDemandMapper.linkProductDemandList(projectId);
+        for (ProductDemandListDO demandListDO : productDemandListDO) {
+            // 查询抄送人
+            List<PersonDO> personDOList = personComponent.select(demandListDO.getId(), PersonTypeEnum.PRODUCT_DEMAND_CC.getCode());
+            demandListDO.setCopier(personDOList.stream().map(PersonDO::getUserName).collect(Collectors.joining(",")));
+        }
         List<ProductDemandVO> productDemandVOList = ProductDemandCopier.INSTANCE.convert(productDemandListDO);
 
         if (CollectionUtils.isNotEmpty(productDemandVOList)) {
