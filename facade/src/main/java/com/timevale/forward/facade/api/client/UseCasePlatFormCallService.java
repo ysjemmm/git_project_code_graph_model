42a0f6ae42316a3db23ec1a477e8c0970900d8cd
yuhua <yuhua@tsign.cn>
2025-09-15 14:00:42 +0800

    feat[调用测试用例平台]：调用测试用例平台接口定义注解
    提交人：雨桦

--- facade/src/main/java/com/timevale/forward/facade/api/client/UseCasePlatFormCallService.java
+++ facade/src/main/java/com/timevale/forward/facade/api/client/UseCasePlatFormCallService.java
@@ -1,9 +1,12 @@
 package com.timevale.forward.facade.api.client;
 
 import com.timevale.footstone.base.model.response.BaseResult;
+import com.timevale.forward.facade.api.MagicValue;
+import com.timevale.mandarin.common.annotation.RestClient;
 
 import java.util.Map;
 
+@RestClient(serviceId = MagicValue.FORWARD_RPC_PREFIX)
 public interface UseCasePlatFormCallService {
     BaseResult queryProjectGroupList(Map<String, Object> params);
 
