b5632e2af3f674199ed2ccf240cc99d2f5a1f8d0
yuhua <yuhua@tsign.cn>
2025-10-28 15:17:58 +0800

    feat[需求规划]：新增单独需求规划接口，修改资源配置
    提交人：雨桦

--- dal/src/main/java/com/timevale/forward/dal/dao/ProductDemandMapper.java
+++ dal/src/main/java/com/timevale/forward/dal/dao/ProductDemandMapper.java
@@ -89,6 +89,14 @@ public interface ProductDemandMapper {
     int update(ProductDemandDO productDemandDO);
 
     /**
+     * 更新单条产品需求资源规划
+     *
+     * @param productDemandDO 产品需求DO
+     * @return int
+     */
+    int updateResourcePlan(ProductDemandDO productDemandDO);
+
+    /**
      * 更新负责人 by id列表
      *
      * @param idList  id列表
