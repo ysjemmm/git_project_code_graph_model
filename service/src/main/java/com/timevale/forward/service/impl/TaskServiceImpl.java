776846db7cb414511abf7ab8fd5bf5d129bff0ec
yuhua <yuhua@tsign.cn>
2025-11-26 16:00:03 +0800

    feat[产品需求]：修改产品需求状态，优化任务实际开始时间赋值
    提交人：雨桦

--- service/src/main/java/com/timevale/forward/service/impl/TaskServiceImpl.java
+++ service/src/main/java/com/timevale/forward/service/impl/TaskServiceImpl.java
@@ -912,8 +912,13 @@ public class TaskServiceImpl implements TaskService {
                         || Objects.equals(projectDO.getOtnPrincipalId(), userInfo.getId());
                 AssertUtil.checkState(authority, "仅项目经理、1-N产研团队负责人允许修改任务的计划时间");
             }
+        }
 
-            // PBG的实际开始实际是根据工时填报情况来的
+        // PBG的实际开始实际是根据工时填报情况来的
+        if ((Objects.nonNull(taskDO.getActualStartDate()) ||
+                Objects.nonNull(newTaskDO.getActualStartDate()))
+                && !Objects.equals(taskDO.getActualStartDate(), newTaskDO.getActualStartDate())) {
+            ProjectDO projectDO = projectMapper.get(taskDO.getProjectId());
             if (ProjectKindEnum.PBG_BASE.getCode().equals(projectDO.getKind())) {
                 newTaskDO.setActualStartDate(taskDO.getActualStartDate());
             }
